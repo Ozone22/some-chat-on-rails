2fc34f1bfbc3f8ce26159d5e8c718ba1dce7eb4f
Ozone22 <willfarm@bk.ru>
2016-07-08 23:31:09 +0300

    Heroku commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'some-chat-on-rails.herokuapp.com' }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
